1e9e42bfb054bf8ece873629ffe9cd22295126a3
roopnsingh <to.roopnsingh@gmail.com>
2025-12-29 00:22:00 +0530

    day two of practice: templates

--- djangoprac/polls/views.py
+++ djangoprac/polls/views.py
@@ -1,7 +1,23 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
-def index(_request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
+def index(request):
+
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+def details(request, question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+def results(request, question_id):
+    response = "You're looking at results of the question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s." %question_id)
 
